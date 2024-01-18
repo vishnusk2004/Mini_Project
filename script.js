d7480cfeeb8d8390fa93c14af8c0e04bb7f146b1
Sravan Kumar <131501151+vishnusk2004@users.noreply.github.com>
2024-01-18 19:22:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ const scroll = new LocomotiveScroll({
+     el: document.querySelector('.main-container'),
+     smooth: true
+ });
+
+var a = document.querySelector("img")
+var b = a.getAttribute("src")
+console.log(b)
